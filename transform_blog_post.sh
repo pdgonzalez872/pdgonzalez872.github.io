a90b57fd9691d22cc51ec1b93823b67860954fd5
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2024-10-25 11:46:34 -0500

    better

--- transform_blog_post.sh
+++ transform_blog_post.sh
@@ -4,10 +4,11 @@ echo "Starting"
 
 file="$1"
 
-sed -i 's|<p>|<p class="my-4">|g' "$file"
-
-sed -i 's|<pre></code>|<pre><code>|g' "$file"
-
-sed -i 's|<pre><code>|<pre class="text-xs"><code>|g' "$file"
+sed -i '' 's|<p>|<p class="my-4">|g' "$file"
+sed -i '' 's|<pre></code>|<pre><code>|g' "$file"
+sed -i '' 's|<pre><code>|<pre class="text-xs"><code>|g' "$file"
+sed -i '' 's|<h3>|<div class="text-2xl font-bold mt-6 mb-6">|g' "$file"
+sed -i '' 's|</h3>|</div>|g' "$file"
+sed -i '' 's|<li>|<li class="mb-2">|g' "$file"
 
 echo "Finished"
