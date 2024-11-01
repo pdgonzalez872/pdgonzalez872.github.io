2462eb0a7ef46e3e6eaed442196d21a6f96df130
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2024-11-01 14:05:30 -0500

    refactor a bit, less obscure

--- lib/mix/tasks/build_static.ex
+++ lib/mix/tasks/build_static.ex
@@ -4,50 +4,43 @@ defmodule Mix.Tasks.BuildStatic do
   @shortdoc "Builds the static content"
 
   def run(_args) do
+    IO.puts("Booting app")
     Mix.Task.run("app.start")
-    IO.puts("Building static site...")
 
+    IO.puts("Start building /docs static")
+
+    IO.puts("Create directory")
     build_dir = "build"
     File.rm_rf!(build_dir)
     File.mkdir_p!(build_dir)
 
+    IO.puts("Create static pages")
     generate_page("/", "index.html", build_dir)
     generate_page("/resume_technical", "resume_technical.html", build_dir)
-    generate_posts(build_dir)
-    copy_assets(build_dir)
 
-    IO.puts("Static site generated in #{build_dir}/")
-  end
-
-  defp generate_page(path, filename, build_dir) do
-    conn = build_conn(path)
-    content = conn.resp_body
-    File.write!(Path.join(build_dir, filename), content)
-    IO.puts("Generated #{filename}")
-  end
-
-  defp generate_posts(build_dir) do
-    posts = PgBlog.Blog.all_posts()
-
-    Enum.each(posts, fn post ->
+    IO.puts("Create posts - add them in priv/posts/")
+    PgBlog.Blog.all_posts()
+    |> Enum.each(fn post ->
       path = "/posts/#{post.id}"
       filename = "#{post.id}.html"
 
       generate_page(path, filename, build_dir)
     end)
-  end
 
-  defp build_conn(path) do
-    conn = Plug.Test.conn(:get, path)
-    PgBlogWeb.Endpoint.call(conn, [])
-  end
-
-  defp copy_assets(build_dir) do
-    # Instead of all assets, maybe just copy what we need, which isn't a lot.
-    # TODO: HERE
+    IO.puts("Copy assets")
     File.cp_r!("priv/static/.", build_dir)
 
-    File.cp("priv/static/favicon.ico", Path.join([build_dir, "images", "favicon.ico"]))
-    |> IO.inspect(label: "cp")
+    IO.puts("Finished building /docs")
+  end
+
+  defp generate_page(path, filename, build_dir) do
+    conn = Plug.Test.conn(:get, path)
+    %{resp_body: body} = PgBlogWeb.Endpoint.call(conn, [])
+
+    build_dir
+    |> Path.join(filename)
+    |> File.write!(body)
+
+    IO.puts("Created #{filename}")
   end
 end
